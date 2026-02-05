3dfa47f67e96a9118dc24650784c4c1114b13a6b
RyanSoar3s <ryansoares.dev@gmail.com>
2026-02-05 18:25:56 -0300

    refactor: change font-xbold to font-bolder

--- src/scripts/components/header/header.js
+++ src/scripts/components/header/header.js
@@ -2,7 +2,7 @@ import { createElement } from "../../core/dom"
 
 const renderHeader = (app) => {
   const content = `
-    <h1 class="header-title text-center font-xbold">O MAPA DA <span class="color-gold">MAESTRIA</span> NA GUITARRA</h1>
+    <h1 class="header-title text-center font-bolder">O mapa da <span class="color-gold">maestria</span> NA GUITARRA</h1>
     <p class="font-light text-center color-gray">De iniciante a guitarrista avançado com um cronograma focado em resultados reais.</p>
 
   `
