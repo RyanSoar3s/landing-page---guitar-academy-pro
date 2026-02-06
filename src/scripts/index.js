2198b8bcca92f579df0fea1a48ca5ad07b3c50a7
RyanSoar3s <ryansoares.dev@gmail.com>
2026-02-06 14:10:15 -0300

    feat: add faq component and handler events

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,11 +1,20 @@
+import events from "./events/event"
 import renderHeader from "./components/header/header"
 import renderCourseSchedule from "./components/course-schedule/course-schedule"
 import renderReviews from "./components/reviews/reviews"
+import renderFaq from "./components/faq/faq"
 
 const initializeApp = () => {
-  const components = [ renderHeader, renderCourseSchedule, renderReviews ]
+  const components = [ 
+    renderHeader, 
+    renderCourseSchedule, 
+    renderReviews,
+    renderFaq
+  
+  ]
 
   components.forEach((component) => component())
+  events()
 
 }
 
