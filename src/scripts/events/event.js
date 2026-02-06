214e131f7b53e0a3b82aff056eca0692c219877b
RyanSoar3s <ryansoares.dev@gmail.com>
2026-02-06 16:40:31 -0300

    feat: add handler eventPurchaseArea

--- src/scripts/events/event.js
+++ src/scripts/events/event.js
@@ -1,6 +1,6 @@
-import { qsAll } from "../core/dom"
+import { qs, qsAll } from "../core/dom"
 
-const events = () => {
+const eventFaq = () => {
   const faqsButton = qsAll(".faqs-button")
   const faqsRes = qsAll(".faqs-res")
 
@@ -12,6 +12,39 @@ const events = () => {
     }
 
   })
+
+}
+
+const eventPurchaseArea = () => {
+  const hourEl = qs("#hour")
+  const minEl = qs("#min")
+  const secEl = qs("#sec")
+
+  const timeRef = 7200
+  let time = timeRef
+
+  let hour = Math.floor(time / 3600)
+  let min = Math.floor((time % 3600) / 60)
+  let sec = Math.floor(time % 60)
+
+  setInterval(() => {
+    hourEl.textContent = hour
+    minEl.textContent = min
+    secEl.textContent = sec
+
+    time = (time - 1 === -1) ? timeRef : time - 1
+
+    hour = Math.floor(time / 3600)
+    min = Math.floor((time % 3600) / 60)
+    sec = Math.floor(time % 60)
+
+  }, 1000)
+
+}
+
+const events = () => {
+  eventFaq()
+  eventPurchaseArea()
 
 }
 
